ac58550a8b0c53993c3ed57c4ab202909d61a7e5
thiagodacosta85 <thiago.costa85@outlook.com>
2020-12-17 14:43:04 -0300

    Primeiro e segundo vídeo finalizados.

---
+++ js/main.js
@@ -0,0 +1,54 @@
+
+var frutas = [{nome:"Maça", cor:"Vermelha"} , {nome:"Uva", cor:"Roxa"}];
+console.log(frutas);
+alert(frutas[1].nome);
+
+/*
+var fruta = {nome:"Maça", cor:"Vermelha"};
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+
+/*
+var lista = ["Maçã", "Pera", "Laranja"];
+lista.push("Uva");
+//lista.pop();
+
+
+console.log(lista);
+console.log(lista.toString());
+console.log(lista.join(" | "));
+*/
+
+/*
+console.log(lista);
+console.log(lista.toString());
+console.log(lista.toString()[0]);
+*/
+
+
+//console.log(lista.length);
+
+//console.log(lista.reverse());
+
+//alert(lista[0]);
+
+/*
+var nome = "Thiago da Costa";
+var n1 = 35;
+var n2 = 10;
+var frase="Japão é o melhor time do mundo." 
+
+//alert("Bem Vindo! " + nome)
+//alert(nome + " tem " + n1 + ".")
+//alert(n1 + n2)
+
+console.log(nome);
+console.log(n1 + n2);
+console.log(frase.replace("Japão", "Brasil"));
+//alert(frase.replace("Japão", "Brasil"));
+alert(frase.toUpperCase());
+//alert(frase.toLocaleLowerCase());
+*/
+
