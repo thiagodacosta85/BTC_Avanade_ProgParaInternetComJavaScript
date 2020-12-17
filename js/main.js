aacd8f05f3b6dbdb126dcdaec5ae581ccc5c5978
thiagodacosta85 <thiago.costa85@outlook.com>
2020-12-17 15:01:13 -0300

    Aula 3 finalizada, anotações no JS.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,57 @@
 
+
+
+
+var d = new Date();
+//console.log(d);
+//alert(d);
+alert(d.getMonth()+1);
+alert(d.getMinutes());
+
+
+/*
+var count;
+
+for(count = 0; count <= 5; count++)
+{
+    alert(count);
+}
+*/
+
+/*
+var count = 0;
+
+while (count <= 5)
+{
+    console.log(count);
+    alert(count);
+    count = count+1;
+}
+*/
+
+
+/*
+var idade = prompt("Qual sua idade?");
+
+//var idade = 17;
+
+
+if(idade >= 18)
+{
+    alert("Maior de idade.");
+}
+    else
+    {
+        alert("Menor de idade.");
+    };
+
+*/
+
+/*
 var frutas = [{nome:"Maça", cor:"Vermelha"} , {nome:"Uva", cor:"Roxa"}];
 console.log(frutas);
 alert(frutas[1].nome);
+*/
 
 /*
 var fruta = {nome:"Maça", cor:"Vermelha"};
